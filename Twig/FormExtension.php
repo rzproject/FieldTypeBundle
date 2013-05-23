422226900c0b436f84d1d30bed055880f4bf68e6
Mell Zamora <mellzamora@outlook.com>
2013-05-23 09:16:47 +0800

    added license

--- Twig/FormExtension.php
+++ Twig/FormExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RzFieldTypeBundle package.
+ *
+ * (c) mell m. zamora <mell@rzproject.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rz\FieldTypeBundle\Twig;
 
 class FormExtension extends \Twig_Extension
